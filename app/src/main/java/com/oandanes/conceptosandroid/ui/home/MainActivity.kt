f3ee78dbaea0fa48d935185de88e8107e5a02761
Manuel Amílcar Martínez Blanco <manuel.martinez@lineadatascan.com>
2024-02-13 08:59:13 -0500

    first Commit

---
+++ app/src/main/java/com/oandanes/conceptosandroid/ui/home/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.oandanes.conceptosandroid.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.oandanes.conceptosandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
